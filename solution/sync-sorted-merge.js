d744feb5ed3164d625a48dbc59cb199bb7ea49a1
kevinpark07 <kevin.park07@yahoo.com>
2021-02-20 10:15:12 -0500

    all entries and sorted

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,9 +2,36 @@
 
 // Print all entries, across all of the sources, in chronological order.
 
+const sortedByDate = (entriesList) => {
+  let sortedEntryList = entriesList.sort((a,b) => a.date - b.date)
+  return sortedEntryList;
+}
+
+
+const getListOfEntries = logSources => {
+  const list = [];
+  logSources.map(source => {
+      while (source.drained === false) {
+        let entry = source.pop();
+        list.push(entry);
+      } 
+    });
+    return list;
+}
+
+const printLogs = (logSources, printer) => {
+  let allEntries = getListOfEntries(logSources);
+  let sortedEntries = sortedByDate(allEntries);
+  let printedEntries = sortedEntries.map(entry => {
+    if(entry.date) {
+    return printer.print(entry)
+    } else { entry.date === undefined }
+  });
+  console.log(printedEntries);
+}
 
 
 module.exports = (logSources, printer) => {
-  console.log(logSources[0].pop());
+  printLogs(logSources, printer);
   return console.log("Sync sort complete.");
 };
