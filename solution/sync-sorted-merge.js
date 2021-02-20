589e091df72dbb7b89a39b14534c322aa2ba9383
kevinpark07 <kevin.park07@yahoo.com>
2021-02-19 21:42:33 -0500

    initial commit

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,6 +2,9 @@
 
 // Print all entries, across all of the sources, in chronological order.
 
+
+
 module.exports = (logSources, printer) => {
+  console.log(logSources[0].pop());
   return console.log("Sync sort complete.");
 };
