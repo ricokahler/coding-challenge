2137419f3030ef1357db0350cb7513f6a4f28b5b
kevinpark07 <kevin.park07@yahoo.com>
2021-02-20 11:18:46 -0500

    challenge number 1 completed

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,6 +2,11 @@
 
 // Print all entries, across all of the sources, in chronological order.
 
+const sortedByDate = (entriesList) => {
+  let sortedEntryList = entriesList.sort((a,b) => a.date - b.date)
+  return sortedEntryList;
+}
+
 
 const getListOfEntries = logSources => {
   const list = [];
@@ -17,10 +22,10 @@ const getListOfEntries = logSources => {
 
 const printLogs = (logSources, printer) => {
   let allEntries = getListOfEntries(logSources);
+  sortedByDate(allEntries);
   allEntries.map(entry => {
       return printer.print(entry);
   });
-  
   return printer.done();
 }
 
